c879f8d941d73a2d023c163536fdbc38a6a38675
Karina Huizar <karinahuizar@gmail.com>
2021-08-03 21:22:40 -0500

    Added the GitHub repo URL.

--- src/App.js
+++ src/App.js
@@ -2,5 +2,22 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://twitter.com/kari_triescode" target="_blank">
+          Karina Huizar
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/karinahuizar/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
