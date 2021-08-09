456a0c2132324a3b3c263fa506591a20070e4558
Karina Huizar <karinahuizar@gmail.com>
2021-08-09 06:08:14 -0500

    Removed the iconUrl

--- src/Weather.js
+++ src/Weather.js
@@ -13,6 +13,7 @@ export default function Weather(props) {
       wind: response.data.wind.speed,
       city: response.data.name,
       humidity: response.data.main.humidity,
+      icon: response.data.weather[0].icon,
       description: response.data.weather[0].description,
       date: new Date(response.data.dt * 1000),
     });
